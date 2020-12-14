83c97cd4b0280ecbd0d5ece2e34f6a125ad7b311
ukkpower <ukkpower@gmail.com>
2020-12-14 23:02:42 +0000

    feat: added append card from json

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,26 +1,47 @@
 const cards = document.querySelectorAll('.card');
-var gameData;
+let gameData = [];
+let cardData = [];
+
+let lives = 0;
+let level = 5;
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
 function flipCard() {
 
-  this.classList.add('flip');
+  //this.classList.add('flip');
 
 }
 
-(function loadGameData () {
+(function startGame () {
     loadJSON(function(response) {
-  // Parse JSON string into object
-    gameData = JSON.parse(response);}, 'gameData.json');
-    console.log(gameData);
+        gameData = JSON.parse(response);}, 
+        'gameData.json');
+
+    loadJSON(function(response) {
+        cardData = JSON.parse(response);}, 
+        'cardData.json');
+
+    console.log(gameData[level-1].lives);
+
+    $.each( cardData, function( key, value ) {
+        $('#game-container').append(`<div class="card">
+            <div class="card-front card-face"><img src="assets/svg/${value.image}"></div>
+            <div class="card-back card-face">?</div>
+        </div>`)
+    });
+
+    $(document).on("click", ".card" , function() {
+        $(this).addClass('flip');
+    });
+
 })();
 
 function loadJSON(callback, file) {   
 
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', `assets/${file}`, false);
+    xobj.open('GET', `assets/data/${file}`, false);
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
             callback(xobj.responseText);
